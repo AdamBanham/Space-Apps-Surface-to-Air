0e032d9994e415ee47a30d0b46919b7a87c35a04
Byron Ho <byronho24@gmail.com>
2019-10-19 15:39:59 +1000

    Calculate angle between coordinates

--- location.py
+++ location.py
@@ -1,4 +1,4 @@
-from math import radians, cos, sin, asin, sqrt
+from math import radians, cos, sin, asin, sqrt, atan2
 
 class Coordinate:
     def __init__(self, latitude, longitude):
@@ -15,8 +15,8 @@ def dist_between_coords(coord1, coord2):
 
     From: https://stackoverflow.com/questions/4913349/haversine-formula-in-python-bearing-and-distance-between-two-gps-points
     """
-    lon1, lat1 = coord1.as_tuple()
-    lon2, lat2 = coord2.as_tuple()
+    lat1, lon1 = coord1.as_tuple()
+    lat2, lon2 = coord2.as_tuple()
     # convert decimal degrees to radians 
     lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
 
@@ -27,3 +27,20 @@ def dist_between_coords(coord1, coord2):
     c = 2 * asin(sqrt(a)) 
     r = 6371 # Radius of earth in kilometers. Use 3956 for miles
     return c * r
+
+def bearing_between_coordinates(coord1, coord2):
+    lat1, lon1 = coord1.as_tuple()
+    lat2, lon2 = coord2.as_tuple()
+    # convert decimal degrees to radians 
+    lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
+
+    dlon = lon2 - lon1 
+    dlat = lat2 - lat1 
+    
+    y = sin(dlon) * cos(lat2)
+    x = cos(lat1) * sin(lat2) - sin(lat1) * cos(lat2) * cos(dlon)
+
+    brng = atan2(y, x)
+    brng = degrees(brng)
+    brng = (brng + 360) % 360
+    return brng
